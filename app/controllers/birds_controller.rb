1a2209c354e5c3271cd11a46b6d2ceb731140295
nluuarch@gmail.com <nluuarch@gmail.com>
2022-01-11 00:07:29 -0800

    Completed code-along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,7 +44,16 @@ class BirdsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+      render json: {}
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 
   def bird_params
     params.permit(:name, :species, :likes)
